27d1d2e6b55c70bc1cbe97b5f8abd526326d0b08
Jeffry Marte Bonifacio <frijey@hotmail.com>
2018-02-16 21:14:00 -0400

    Formulario de Agregar Equipos

---
+++ Gestion de Equipos/Agregar Equipo.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Gestion_de_Equipos
+{
+    partial class Agregar_Equipo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btpnew = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btpdelete = new System.Windows.Forms.Button();
+            this.btnactualizarservicio = new System.Windows.Forms.Button();
+            this.tbdias = new System.Windows.Forms.TextBox();
+            this.lblparticipante = new System.Windows.Forms.Label();
+            this.tbpprecio = new System.Windows.Forms.TextBox();
+            this.lblestado = new System.Windows.Forms.Label();
+            this.tbpnombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbpcodigo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblmarca = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btpnew
+            // 
+            this.btpnew.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btpnew.Location = new System.Drawing.Point(110, 274);
+            this.btpnew.Name = "btpnew";
+            this.btpnew.Size = new System.Drawing.Size(102, 39);
+            this.btpnew.TabIndex = 67;
+            this.btpnew.Text = "Nuevo";
+            this.btpnew.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Palatino Linotype", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label8.Location = new System.Drawing.Point(92, 20);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(216, 37);
+            this.label8.TabIndex = 72;
+            this.label8.Text = "Agregar Equipo";
+            // 
+            // btpdelete
+            // 
+            this.btpdelete.Font = new System.Drawing.Font("Palatino Linotype", 12F);
+            this.btpdelete.Location = new System.Drawing.Point(190, 235);
+            this.btpdelete.Name = "btpdelete";
+            this.btpdelete.Size = new System.Drawing.Size(93, 33);
+            this.btpdelete.TabIndex = 66;
+            this.btpdelete.Text = "Eliminar";
+            this.btpdelete.UseVisualStyleBackColor = true;
+            // 
+            // btnactualizarservicio
+            // 
+            this.btnactualizarservicio.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnactualizarservicio.Location = new System.Drawing.Point(78, 235);
+            this.btnactualizarservicio.Name = "btnactualizarservicio";
+            this.btnactualizarservicio.Size = new System.Drawing.Size(106, 33);
+            this.btnactualizarservicio.TabIndex = 65;
+            this.btnactualizarservicio.Text = "Guardar";
+            this.btnactualizarservicio.UseVisualStyleBackColor = true;
+            // 
+            // tbdias
+            // 
+            this.tbdias.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.tbdias.Location = new System.Drawing.Point(155, 196);
+            this.tbdias.Name = "tbdias";
+            this.tbdias.Size = new System.Drawing.Size(112, 23);
+            this.tbdias.TabIndex = 64;
+            // 
+            // lblparticipante
+            // 
+            this.lblparticipante.AutoSize = true;
+            this.lblparticipante.Font = new System.Drawing.Font("Tahoma", 14.75F, System.Drawing.FontStyle.Bold);
+            this.lblparticipante.Location = new System.Drawing.Point(16, 193);
+            this.lblparticipante.Name = "lblparticipante";
+            this.lblparticipante.Size = new System.Drawing.Size(133, 24);
+            this.lblparticipante.TabIndex = 71;
+            this.lblparticipante.Text = "Participante";
+            // 
+            // tbpprecio
+            // 
+            this.tbpprecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.tbpprecio.Location = new System.Drawing.Point(154, 165);
+            this.tbpprecio.Name = "tbpprecio";
+            this.tbpprecio.Size = new System.Drawing.Size(112, 23);
+            this.tbpprecio.TabIndex = 63;
+            // 
+            // lblestado
+            // 
+            this.lblestado.AutoSize = true;
+            this.lblestado.Font = new System.Drawing.Font("Tahoma", 14.75F, System.Drawing.FontStyle.Bold);
+            this.lblestado.Location = new System.Drawing.Point(69, 165);
+            this.lblestado.Name = "lblestado";
+            this.lblestado.Size = new System.Drawing.Size(77, 24);
+            this.lblestado.TabIndex = 70;
+            this.lblestado.Text = "Estado";
+            // 
+            // tbpnombre
+            // 
+            this.tbpnombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.tbpnombre.Location = new System.Drawing.Point(153, 105);
+            this.tbpnombre.Name = "tbpnombre";
+            this.tbpnombre.Size = new System.Drawing.Size(238, 23);
+            this.tbpnombre.TabIndex = 62;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 14.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(57, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 24);
+            this.label2.TabIndex = 69;
+            this.label2.Text = "Nombre";
+            // 
+            // tbpcodigo
+            // 
+            this.tbpcodigo.Enabled = false;
+            this.tbpcodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.tbpcodigo.Location = new System.Drawing.Point(153, 73);
+            this.tbpcodigo.Name = "tbpcodigo";
+            this.tbpcodigo.Size = new System.Drawing.Size(90, 23);
+            this.tbpcodigo.TabIndex = 61;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 14.75F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(68, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 24);
+            this.label1.TabIndex = 68;
+            this.label1.Text = "Código";
+            // 
+            // lblmarca
+            // 
+            this.lblmarca.AutoSize = true;
+            this.lblmarca.Font = new System.Drawing.Font("Tahoma", 14.75F, System.Drawing.FontStyle.Bold);
+            this.lblmarca.Location = new System.Drawing.Point(74, 134);
+            this.lblmarca.Name = "lblmarca";
+            this.lblmarca.Size = new System.Drawing.Size(72, 24);
+            this.lblmarca.TabIndex = 70;
+            this.lblmarca.Text = "Marca";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.textBox1.Location = new System.Drawing.Point(153, 134);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 23);
+            this.textBox1.TabIndex = 63;
+            // 
+            // Agregar_Equipo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 327);
+            this.Controls.Add(this.btpnew);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btpdelete);
+            this.Controls.Add(this.btnactualizarservicio);
+            this.Controls.Add(this.tbdias);
+            this.Controls.Add(this.lblparticipante);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblmarca);
+            this.Controls.Add(this.tbpprecio);
+            this.Controls.Add(this.lblestado);
+            this.Controls.Add(this.tbpnombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbpcodigo);
+            this.Controls.Add(this.label1);
+            this.Name = "Agregar_Equipo";
+            this.Text = "Agregar Equipo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btpnew;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btpdelete;
+        private System.Windows.Forms.Button btnactualizarservicio;
+        private System.Windows.Forms.TextBox tbdias;
+        private System.Windows.Forms.Label lblparticipante;
+        private System.Windows.Forms.TextBox tbpprecio;
+        private System.Windows.Forms.Label lblestado;
+        private System.Windows.Forms.TextBox tbpnombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbpcodigo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblmarca;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ Gestion de Equipos/Consultar.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Gestion_de_Equipos
+{
+    partial class Consultar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbbuscar = new System.Windows.Forms.TextBox();
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.dgvequipos = new System.Windows.Forms.DataGridView();
+            this.cedula = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sueldobruto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cargo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.cbtipobusqueda = new System.Windows.Forms.ComboBox();
+            this.btnsalida = new System.Windows.Forms.Button();
+            this.btnentrada = new System.Windows.Forms.Button();
+            this.btnmostrartodo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvequipos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbbuscar
+            // 
+            this.tbbuscar.BackColor = System.Drawing.Color.AliceBlue;
+            this.tbbuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbbuscar.Location = new System.Drawing.Point(245, 13);
+            this.tbbuscar.Name = "tbbuscar";
+            this.tbbuscar.Size = new System.Drawing.Size(204, 29);
+            this.tbbuscar.TabIndex = 75;
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.BackColor = System.Drawing.Color.Cornsilk;
+            this.btnbuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnbuscar.Location = new System.Drawing.Point(455, 7);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(111, 38);
+            this.btnbuscar.TabIndex = 76;
+            this.btnbuscar.Text = "Buscar";
+            this.btnbuscar.UseVisualStyleBackColor = false;
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnagregar.Location = new System.Drawing.Point(572, 6);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(126, 39);
+            this.btnagregar.TabIndex = 79;
+            this.btnagregar.Text = "Agregar Equipo (+)";
+            this.btnagregar.UseVisualStyleBackColor = false;
+            // 
+            // dgvequipos
+            // 
+            this.dgvequipos.AllowUserToAddRows = false;
+            this.dgvequipos.AllowUserToDeleteRows = false;
+            this.dgvequipos.AllowUserToResizeColumns = false;
+            this.dgvequipos.AllowUserToResizeRows = false;
+            this.dgvequipos.BackgroundColor = System.Drawing.Color.Azure;
+            this.dgvequipos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvequipos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.cedula,
+            this.Nombre,
+            this.sueldobruto,
+            this.cargo});
+            this.dgvequipos.GridColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.dgvequipos.Location = new System.Drawing.Point(15, 51);
+            this.dgvequipos.MultiSelect = false;
+            this.dgvequipos.Name = "dgvequipos";
+            this.dgvequipos.ReadOnly = true;
+            this.dgvequipos.RowHeadersVisible = false;
+            this.dgvequipos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvequipos.Size = new System.Drawing.Size(683, 319);
+            this.dgvequipos.TabIndex = 78;
+            // 
+            // cedula
+            // 
+            this.cedula.HeaderText = "Código";
+            this.cedula.Name = "cedula";
+            this.cedula.ReadOnly = true;
+            this.cedula.Width = 60;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre del Equipo";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            this.Nombre.Width = 260;
+            // 
+            // sueldobruto
+            // 
+            this.sueldobruto.HeaderText = "Estado";
+            this.sueldobruto.Name = "sueldobruto";
+            this.sueldobruto.ReadOnly = true;
+            this.sueldobruto.Width = 150;
+            // 
+            // cargo
+            // 
+            this.cargo.HeaderText = "Participante";
+            this.cargo.Name = "cargo";
+            this.cargo.ReadOnly = true;
+            this.cargo.Width = 220;
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.AutoSize = true;
+            this.lbltitulo.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.Location = new System.Drawing.Point(9, 10);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(118, 32);
+            this.lbltitulo.TabIndex = 80;
+            this.lbltitulo.Text = "Consultar";
+            // 
+            // cbtipobusqueda
+            // 
+            this.cbtipobusqueda.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbtipobusqueda.FormattingEnabled = true;
+            this.cbtipobusqueda.Items.AddRange(new object[] {
+            "Participante",
+            "Equipos",
+            "Empleados"});
+            this.cbtipobusqueda.Location = new System.Drawing.Point(129, 17);
+            this.cbtipobusqueda.Name = "cbtipobusqueda";
+            this.cbtipobusqueda.Size = new System.Drawing.Size(110, 21);
+            this.cbtipobusqueda.TabIndex = 83;
+            // 
+            // btnsalida
+            // 
+            this.btnsalida.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.btnsalida.Font = new System.Drawing.Font("Palatino Linotype", 12F);
+            this.btnsalida.Location = new System.Drawing.Point(507, 383);
+            this.btnsalida.Name = "btnsalida";
+            this.btnsalida.Size = new System.Drawing.Size(155, 38);
+            this.btnsalida.TabIndex = 81;
+            this.btnsalida.Text = "Dar Salida";
+            this.btnsalida.UseVisualStyleBackColor = false;
+            // 
+            // btnentrada
+            // 
+            this.btnentrada.BackColor = System.Drawing.Color.Azure;
+            this.btnentrada.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnentrada.Location = new System.Drawing.Point(36, 383);
+            this.btnentrada.Name = "btnentrada";
+            this.btnentrada.Size = new System.Drawing.Size(155, 38);
+            this.btnentrada.TabIndex = 82;
+            this.btnentrada.Text = "Dar Entrada";
+            this.btnentrada.UseVisualStyleBackColor = false;
+            // 
+            // btnmostrartodo
+            // 
+            this.btnmostrartodo.BackColor = System.Drawing.Color.White;
+            this.btnmostrartodo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnmostrartodo.Location = new System.Drawing.Point(244, 383);
+            this.btnmostrartodo.Name = "btnmostrartodo";
+            this.btnmostrartodo.Size = new System.Drawing.Size(214, 38);
+            this.btnmostrartodo.TabIndex = 77;
+            this.btnmostrartodo.Text = "Mostrar Todos";
+            this.btnmostrartodo.UseVisualStyleBackColor = false;
+            // 
+            // Consultar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 430);
+            this.Controls.Add(this.tbbuscar);
+            this.Controls.Add(this.btnbuscar);
+            this.Controls.Add(this.btnagregar);
+            this.Controls.Add(this.dgvequipos);
+            this.Controls.Add(this.lbltitulo);
+            this.Controls.Add(this.cbtipobusqueda);
+            this.Controls.Add(this.btnsalida);
+            this.Controls.Add(this.btnentrada);
+            this.Controls.Add(this.btnmostrartodo);
+            this.Name = "Consultar";
+            this.Text = "Consultar";
+            this.Load += new System.EventHandler(this.Consultar_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvequipos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbbuscar;
+        private System.Windows.Forms.Button btnbuscar;
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.DataGridView dgvequipos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cedula;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sueldobruto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cargo;
+        private System.Windows.Forms.Label lbltitulo;
+        private System.Windows.Forms.ComboBox cbtipobusqueda;
+        private System.Windows.Forms.Button btnsalida;
+        private System.Windows.Forms.Button btnentrada;
+        private System.Windows.Forms.Button btnmostrartodo;
+    }
+}
